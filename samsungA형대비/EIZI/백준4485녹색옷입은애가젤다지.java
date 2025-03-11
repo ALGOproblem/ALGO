8a47a3394fa4bd619e9c2f1e8960c07d268940d0
easy-min <100506494+easy-min@users.noreply.github.com>
2025-03-12 08:12:03 +0900

    Update 백준4485녹색옷입은애가젤다지.java

--- samsungA형대비/EIZI/백준4485녹색옷입은애가젤다지.java
+++ samsungA형대비/EIZI/백준4485녹색옷입은애가젤다지.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -11,6 +12,8 @@ import java.util.StringTokenizer;
  * 3. "0-1 BFS" 방식으로 풀려는 접근 방식 실패
 	하지만 이 문제는 단순한 0-1 가중치 문제가 아니라, 가중치가 0~9까지 다양하게 존재하는 문제!
 	즉, 일반적인 BFS(0-1 BFS) 방식이 아니라, 다익스트라(우선순위 큐)를 사용해야 함.
+	
+	출력 형식이 Problem인데 problem 소문자로 씀
  */
 public class 백준4485녹색옷입은애가젤다지 {
 	static int N;
@@ -33,7 +36,7 @@ public class 백준4485녹색옷입은애가젤다지 {
         			arr[i][j] = Integer.parseInt(st.nextToken()); // 0이상 9이하 한 자리수
         		}
         	}//입력
-        	System.out.println("problem "+time+": "+dijkstra());
+        	System.out.println("Problem "+time+": "+dijkstra());
         	
         }
 	}
@@ -42,7 +45,6 @@ public class 백준4485녹색옷입은애가젤다지 {
 		int y;
 		int cost;
 		public Node() {
-			// TODO Auto-generated constructor stub
 		}
 		public Node(int x, int y, int cost) {
 			this.x = x;
@@ -60,12 +62,13 @@ public class 백준4485녹색옷입은애가젤다지 {
 		for (int i = 0; i<N; i++) Arrays.fill(dist[i], INF); // INF로 초기화
 		dist[0][0] = arr[0][0];
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(0, 0, arr[0][0])); // x, y, dist
+		pq.offer(new Node(0, 0, arr[0][0])); // x, y, dist
 		while(!pq.isEmpty()) {
 			Node current = pq.poll();
 			int curx = current.x;
 			int cury = current.y;
 			int cost = current.cost;
+			if (cost > dist[curx][cury]) continue;
 			for (int i = 0; i<4; i++) {
 				int nextx = curx + dirx[i];
 				int nexty = cury + diry[i];
@@ -73,8 +76,8 @@ public class 백준4485녹색옷입은애가젤다지 {
 					int nextCost = cost + arr[nextx][nexty];
 					if (nextCost < dist[nextx][nexty]) {
 						dist[nextx][nexty] = nextCost;
-						pq.add(new Node(nextx, nexty, nextCost));
-					}
+						pq.offer(new Node(nextx, nexty, nextCost));
+					}	
 				}
 			}
 			
